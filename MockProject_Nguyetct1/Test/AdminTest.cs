60b440ece86fe1b04f74b17625e2e8212f5c3a67
MoonCao2211 <cao.nguyet2811@gmail.com>
2023-05-29 00:38:27 +0700

    update

--- MockProject_Nguyetct1/Test/AdminTest.cs
+++ MockProject_Nguyetct1/Test/AdminTest.cs
@@ -63,21 +63,21 @@ namespace MockProject_Nguyetct1.Test
             message.Should().Be("Success");
         }
 
-        // [Test, Description("TC02: Update job title pass")]
-        // public void TC02UPDATEJOBTITLEPASS()
-        // {
-        //     adminPage.ClickAdminBtn();
-        //     adminPage.ClickJobBtn();
-        //     adminPage.ClickJobTitleBtn();
-        //     //Update Job
+        [Test, Description("TC02: Update job title pass")]
+        public void TC02UPDATEJOBTITLEPASS()
+        {
+            adminPage.ClickAdminBtn();
+            adminPage.ClickJobBtn();
+            adminPage.ClickJobTitleBtn();
+            //Update Job
 
-        //     adminPage.UpdateJobs(_job);
-        //     //Verify: Get message -> verify
-        //     message = adminPage.GetMessageAdminPage();
-        //     message.Should().Be("Success");
+            adminPage.UpdateJobs(_job);
+            //Verify: Get message -> verify
+            message = adminPage.GetMessageAdminPage();
+            message.Should().Be("Success");
 
 
-        // }
+        }
 
         // [Test, TestCaseSource(typeof(AdminPage), nameof(AdminPage.GetJobsUpdate)), Description("TC03: Delete job title pass")]
         // public void TC02DELETEJOBTITLEPASS(Job job)
